25a3a06e56b1bc06c3d874fe33d95dfa9fc7f4ab
uptheshipcreek <northenore@gmail.com>
2023-04-03 19:18:38 +0300

    Finally solved using an accumulator instead of the given preset

---
+++ assignment/testing.pl
@@ -0,0 +1,80 @@
+head([H|_],H).
+
+min(X,Y,Y):-
+    X>=Y.
+min(X,Y,X):-
+    X<Y.
+
+max(X,Y,X):-
+    X>=Y.
+max(X,Y,Y):-
+    X<Y.
+
+gatherActivities(AIds):-
+    gatherActivities(AIds,[]),!.
+gatherActivities(AIds,B):-
+    activity(Id,_),
+    \+member(Id,B),
+    gatherActivities(AIds,[Id|B]).
+gatherActivities(AIds,AIds).
+
+pickWorker(N,N).
+pickWorker(W,N):-
+    N>1,
+    N0 is N - 1,
+    pickWorker(W,N0).
+
+gatherWorkerActivities(_,[],[]).
+gatherWorkerActivities(PId,[AId-PId|Assigment],[AId|Activities]):-
+    gatherWorkerActivities(PId,Assigment,Activities),!.
+gatherWorkerActivities(PId,[_-DId|Assigment],Activities):-
+    PId =\= DId,
+    gatherWorkerActivities(PId,Assigment,Activities),!.
+
+findMaxIndex(Assignment,Max):-
+    findMaxIndex(Assignment,Assignment,0,Max).
+
+findMaxIndex(_,[],Max,Max).
+findMaxIndex(As,[_-Index|Assignment],Temp,Max):-
+    max(Index,Temp,NT),
+    findMaxIndex(As,Assignment,NT,Max),!.
+
+addTimes(APIds,R):-addTimes(APIds,APIds,R,0).
+addTimes(_,[],R,R):-!.
+addTimes(APIds,[H|A],R,C):-
+    activity(H,act(Start,Finish)),
+    Time is Finish - Start,
+    NC is C + Time,
+    addTimes(APIds,A,R,NC).
+
+check(PId,AId,Assignment,MaxTime):-
+    activity(AId, act(Ab, Ae)),
+    gatherWorkerActivities(PId, Assignment, APIds),
+    NewTime is Ae - Ab,
+    addTimes(APIds,SoFarTime),
+    OverallTime is NewTime + SoFarTime,
+    OverallTime =< MaxTime,
+    valid(Ab, Ae, APIds).
+
+assignment(NPersons,MaxTime,Assignment,Assignment):-
+    gatherActivities(AIds),
+    assign(AIds,NPersons,MaxTime,Assignment).
+
+assign(AIds,NPersons,MaxTime,Assignment):-
+    assign(AIds,AIds,NPersons,MaxTime,[],Assignment).
+assign(_,[],_,_,Assignment,Assignment).
+assign(A,[AId|AIds],NPersons,MaxTime,SoFarAssignment,Assignment):-
+    findMaxIndex(SoFarAssignment,SoFarMax), %we must not assign the "(n+1)-nth" worker before the "n-nth", if we are to exclude mirror solutions. If we need someone different from the previous "(n-1)" workers, we shall always call him "n-nth".
+    succ(SoFarMax,S),
+    min(S,NPersons,M),
+    pickWorker(PId,M),
+    check(PId,AId,SoFarAssignment,MaxTime),
+    assign(A,AIds,NPersons,MaxTime,[AId-PId|SoFarAssignment],Assignment).
+
+valid(_,_,[]).
+valid(Ab1,Ae1,[APId|APIds]):-
+    activity(APId,act(Ab2,Ae2)),
+    (
+        Ab1 > Ae2; Ab2 > Ae1
+    ),
+    valid(Ab1,Ae1,APIds).
